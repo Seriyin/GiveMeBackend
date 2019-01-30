6a0dd575c29b67a25691e279a9937a4781d46d13
André Diogo <redan.blue27@gmail.com>
2019-01-30 22:20:13 +0000

    Separates session logic and instance from models.

---
+++ sessions/sessions.go
@@ -0,0 +1,23 @@
+package sessions
+
+import (
+	"github.com/gorilla/sessions"
+	"os"
+)
+
+var (
+	SessionStore sessions.Store
+)
+
+func init() {
+	cookie_secret := os.Getenv("COOKIE_SECRET")
+	// [START sessions]
+	// Configure storage method for session-wide information.
+	// Update "something-very-secret" with a hard to guess string or byte sequence.
+	cookieStore := sessions.NewCookieStore([]byte(cookie_secret))
+	cookieStore.Options = &sessions.Options{
+		HttpOnly: true,
+	}
+	SessionStore = cookieStore
+	// [END sessions]
+}
