b0a0782b854a6827a771560b0b7f13d1ccbe38b2
André Diogo <redan.blue27@gmail.com>
2019-01-31 23:12:32 +0000

    Adds notifications for request and reminder on new user join.

--- firebase/messaging/messaging.go
+++ firebase/messaging/messaging.go
@@ -1,9 +1,56 @@
 package messaging
 
 import (
-	"github.com/Seriyin/GibMe-backend/firebase"
+	"firebase.google.com/go/messaging"
+	"fmt"
 )
 
-var (
-	Mes = firebase.GetMessaging()
-)
+var ()
+
+func generateRequestNotification(
+	token string,
+	amountUnits int64,
+	amountCents int32,
+	deliveredFrom string,
+) *messaging.Message {
+	return &messaging.Message{
+		Android: &messaging.AndroidConfig{
+			Priority: "normal",
+			Notification: &messaging.AndroidNotification{
+				Title: "Debt Notification",
+				Body: fmt.Sprintf(
+					"You owe %v.%v to %v",
+					amountUnits,
+					amountCents,
+					deliveredFrom,
+				),
+				Color: "#161119",
+			},
+			RestrictedPackageName: "com.giveme.pei.givemeapp",
+		},
+		Token: token,
+	}
+}
+
+func generateNewUserRequest(
+	toCheckRemind string,
+	token string,
+	toRemind string,
+) *messaging.Message {
+	return &messaging.Message{
+		Android: &messaging.AndroidConfig{
+			Priority: "normal",
+			Notification: &messaging.AndroidNotification{
+				Title: "User Joined Notification",
+				Body: fmt.Sprintf(
+					"%v, A user who owes you has joined GiveMe. Remind %v them of their debt?",
+					toCheckRemind,
+					toRemind,
+				),
+				Color: "#161119",
+			},
+			RestrictedPackageName: "com.giveme.pei.givemeapp",
+		},
+		Token: token,
+	}
+}
