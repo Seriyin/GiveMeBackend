73d01f74d498f757fd47248c8aff9d039b6243f9
Nuno Vieira <nunocv96@gmail.com>
2019-02-07 16:56:20 +0000

    request monetary transfer function

--- request/fn.go
+++ request/fn.go
@@ -1,6 +1,8 @@
 package request
 
 import (
+	"strings"
+	"json"
 	"context"
 	"github.com/Seriyin/GibMe-backend/config/firebase"
 	"github.com/Seriyin/GibMe-backend/config/firebase/firestore"
@@ -12,4 +14,17 @@ func Request(
 	ctx context.Context,
 	e firestore.Event,
 ) {
+	var moneteryT MonetaryTransfer //Monetary Structure object
+	err := json.Unmarshal(e.Value.Fields, &moneteryT) //Json obejct to Monetary Structure
+	debtor := moneteryT.To //Debtor present in the To value on the Monetary Stuct
+
+	/*
+	*Use the debotr id to split the path
+	*so it gets the path next to the credor ID 
+	*to be replaced by the debtor ID, so it inserts on the correct collection
+	*/
+	splitvalue := "/"+moneteryT.From+"/"
+	path := strings.Split(e.Value.Name, splivalue)[1]
+
+	db.setMonetaryTransfer(debtor, newmonetaryT, path)
 }
