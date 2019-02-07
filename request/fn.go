e717db95f0b321a4a116012ca8eccd3a8cceb03b
André Diogo <redan.blue27@gmail.com>
2019-02-07 17:16:40 +0000

    Adds minor fixes.

--- request/fn.go
+++ request/fn.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/Seriyin/GibMe-backend/config/datastore"
 	"github.com/Seriyin/GibMe-backend/config/firebase"
 	"github.com/Seriyin/GibMe-backend/config/firebase/firestore"
 )
@@ -16,19 +17,17 @@ func Request(
 	ctx context.Context,
 	e firestore.Event,
 ) error {
-	var monetaryT MonetaryTransfer                    //Monetary Structure object
-	err := json.Unmarshal(e.Value.Fields, &monetaryT) //Json object to Monetary Structure
-	debtor := monetaryT.To                            //Debtor present in the To value on the Monetary Struct
+	var monetaryT datastore.MonetaryTransfer          // Monetary Structure object
+	err := json.Unmarshal(e.Value.Fields, &monetaryT) // Json object to Monetary Structure
+	debtor := monetaryT.To                            // Debtor present in the To value on the Monetary Struct
 
-	/*
-	*Use the debtor id to split the path
-	*so it gets the path next to the creditor ID
-	*to be replaced by the debtor ID, so it inserts on the correct collection
-	 */
+	// Use the debtor id to split the path
+	// so it gets the path next to the creditor ID
+	// to be replaced by the debtor ID, so it inserts on the correct collection.
 	splitvalue := "/" + monetaryT.From + "/"
 	path := strings.Split(e.Value.Name, splitvalue)[1]
 
-	_, err = db.setMonetaryTransfer(debtor, newmonetaryT, path)
+	_, err = db.SetMonetaryTransfer(debtor, &monetaryT, path)
 	if err != nil {
 		return fmt.Errorf("Set: %v", err)
 	}
