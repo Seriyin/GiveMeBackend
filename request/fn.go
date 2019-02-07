af1ae7ecb55e81f0a7ed9b334272998448e1ccc9
Nuno Vieira <nunocv96@gmail.com>
2019-02-07 17:08:13 +0000

    encoding/json and error management

--- request/fn.go
+++ request/fn.go
@@ -1,9 +1,11 @@
 package request
 
 import (
-	"strings"
-	"json"
 	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+
 	"github.com/Seriyin/GibMe-backend/config/firebase"
 	"github.com/Seriyin/GibMe-backend/config/firebase/firestore"
 )
@@ -13,18 +15,22 @@ var db = firebase.GetDB()
 func Request(
 	ctx context.Context,
 	e firestore.Event,
-) {
-	var monetaryT MonetaryTransfer //Monetary Structure object
+) error {
+	var monetaryT MonetaryTransfer                    //Monetary Structure object
 	err := json.Unmarshal(e.Value.Fields, &monetaryT) //Json object to Monetary Structure
-	debtor := monetaryT.To //Debtor present in the To value on the Monetary Struct
+	debtor := monetaryT.To                            //Debtor present in the To value on the Monetary Struct
 
 	/*
 	*Use the debtor id to split the path
-	*so it gets the path next to the creditor ID 
+	*so it gets the path next to the creditor ID
 	*to be replaced by the debtor ID, so it inserts on the correct collection
-	*/
-	splitvalue := "/"+monetaryT.From+"/"
+	 */
+	splitvalue := "/" + monetaryT.From + "/"
 	path := strings.Split(e.Value.Name, splitvalue)[1]
 
-	db.setMonetaryTransfer(debtor, newmonetaryT, path)
+	_, err = db.setMonetaryTransfer(debtor, newmonetaryT, path)
+	if err != nil {
+		return fmt.Errorf("Set: %v", err)
+	}
+	return nil
 }
