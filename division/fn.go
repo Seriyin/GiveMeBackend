48631bc965251d4735bc074e59659c7f59c3892f
Nuno Vieira <nunocv96@gmail.com>
2019-02-08 16:58:50 +0000

    monetary transfers amount calculation

--- division/fn.go
+++ division/fn.go
@@ -2,6 +2,13 @@ package division
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Seriyin/GibMe-backend/config/datastore"
 	"github.com/Seriyin/GibMe-backend/config/firebase"
 	"github.com/Seriyin/GibMe-backend/config/firebase/firestore"
 )
@@ -11,5 +18,33 @@ var db = firebase.GetDB()
 func Division(
 	ctx context.Context,
 	e firestore.Event,
-) {
+) error {
+	var groupT datastore.GroupTransfer
+	time := time.Now()
+	err := json.Unmarshal(e.Value.Fields, &groupT)
+	if err != nil {
+		return err
+	}
+	totalvalue := groupT.AmountUnit*100 + groupT.AmountCents
+	var dividedvalue float64
+	var newAmountUnit int64
+	var newAmountCents int64
+	if groupT.Included {
+		dividedvalue = float64(totalvalue/(len(groupT.Tos)+1))
+		st := fmt.Sprintf("%f", dividedvalue)
+		sp := strings.Split(a, ".")
+		newAmountUnit = strconv.ParseInt(sp[0], 10, 64)
+		newAmountCents = strconv.ParseInt(sp[1], 10, 64)
+	}
+	else {
+		dividedvalue = float64(totalvalue/(len(groupT.Tos))
+		st := fmt.Sprintf("%f", dividedvalue)
+		sp := strings.Split(a, ".")
+		newAmountUnit = strconv.ParseInt(sp[0], 10, 64)
+		newAmountCents = strconv.ParseInt(sp[1], 10, 64)
+	}
+	var monetaryTs []*datastore.MonetaryTransfer 
+	for _, to := range groupT.Tos {
+		
+	} 
 }
--- division/go.mod
+++ division/go.mod
@@ -1,5 +1,10 @@
 module github.com/Seriyin/GibMe-backend/division
 
-require github.com/Seriyin/GibMe-backend/config v0.0.0
+require (
+	github.com/Seriyin/GibMe-backend/config v0.0.0
+	google.golang.org/api v0.1.0 // indirect
+	google.golang.org/genproto v0.0.0-20190201180003-4b09977fb922 // indirect
+	google.golang.org/grpc v1.18.0 // indirect
+)
 
 replace github.com/Seriyin/GibMe-backend/config => ../config/
