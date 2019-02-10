23103c748d7ced7619fbabd431b6062aebece02d
André Diogo <redan.blue27@gmail.com>
2019-02-10 21:40:30 +0000

    Substitutes function into two functional units.
    Splits path accurately and extracts ids as necessary.

--- request/fn.go
+++ request/fn.go
@@ -24,31 +24,58 @@ func Request(
 	if err != nil {
 		return err
 	}
-	debtor := monetaryT.To // Debtor present in the To value on the Monetary Struct
 
-	// Use the debtor id to split the path
-	// so it gets the path next to the creditor ID
-	// to be replaced by the debtor ID, so it inserts on the correct collection.
-	splitvalue := "/" + monetaryT.From + "/"
-	path := strings.Split(e.Value.Name, splitvalue)[1]
-
-	_, err = db.SetMonetaryTransfer(debtor, &monetaryT, path)
+	id, profile, err := db.GetProfileByPhoneNumber(ctx, monetaryT.To)
 	if err != nil {
 		return err
 	}
 
+	err = extractAndReplaceMonetaryTransfer(ctx, id, &monetaryT, e.Value.Name)
+	if err != nil {
+		return err
+	}
+
+	err = produceAndSendNotification(ctx, profile, &monetaryT)
+	return err
+}
+
+func extractAndReplaceMonetaryTransfer(
+	ctx context.Context,
+	id string,
+	mon *datastore.MonetaryTransfer,
+	networkPath string,
+) error {
+
+	// Use the debtor id to split the path
+	// so it gets the path next to the creditor ID
+	// to be replaced by the debtor ID, so it inserts on the correct collection.
+
+	//Split at gcpstuff in [0] and full db path in [1].
+	path := strings.Split(networkPath, "/documents/")[1]
+	//Split by collection and extract [1] which should be fromId.
+	fromId := strings.Split(path, "/")[1]
+	dbPath := strings.Replace(path, fromId, id, 1)
+
+	_, err := db.SetMonetaryTransfer(ctx, id, mon, dbPath)
+	return err
+}
+
+func produceAndSendNotification(
+	ctx context.Context,
+	profile *datastore.Profile,
+	transfer *datastore.MonetaryTransfer,
+) error {
 	//generate notification message
-	token := "" //IMPLEMENT ME
+	token := profile.UserId.Token
 	message := messaging.GenerateRequestNotification(
 		token,
-		monetaryT.AmountUnit,
-		monetaryT.AmountCents,
-		monetaryT.Currency,
-		monetaryT.From,
+		transfer.AmountUnit,
+		transfer.AmountCents,
+		transfer.Currency,
+		transfer.From,
 	)
 
-	var str string
-	str, err = mesClient.Send(ctx, message)
+	str, err := mesClient.Send(ctx, message)
 	if err != nil {
 		log.Print(str)
 	}
