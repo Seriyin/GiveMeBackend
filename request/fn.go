aa60fd3610978fc4fc767d4b93264899ecfb856c
André Diogo <redan.blue27@gmail.com>
2019-02-11 15:50:23 +0000

    Cleans up request via common path functionality.

--- request/fn.go
+++ request/fn.go
@@ -3,13 +3,12 @@ package request
 import (
 	"context"
 	"encoding/json"
-	"log"
-	"strings"
-
 	"github.com/Seriyin/GibMe-backend/config/datastore"
 	"github.com/Seriyin/GibMe-backend/config/firebase"
 	"github.com/Seriyin/GibMe-backend/config/firebase/firestore"
 	"github.com/Seriyin/GibMe-backend/config/firebase/messaging"
+	"github.com/Seriyin/GibMe-backend/config/firebase/paths"
+	"log"
 )
 
 var db = firebase.GetDB()
@@ -27,38 +26,34 @@ func Request(
 
 	// If no profile can be gathered, the user may not exist.
 	// Either by network error or profile not existing, must return.
-	id, profile, err := db.GetProfileByPhoneNumber(ctx, monetaryT.To)
+	profile, err := db.GetProfileByPhoneNumber(
+		ctx,
+		monetaryT.To,
+	)
 	if err != nil {
 		return err
 	}
 
-	err = extractAndReplaceMonetaryTransfer(ctx, id, &monetaryT, e.Value.Name)
+	dbPath := paths.ExtractAndReplaceMethodIdAndDatePath(
+		profile.Id,
+		e.Value.Name,
+	)
+
+	_, err = db.SetMonetaryTransfer(
+		ctx,
+		profile.Id,
+		&monetaryT,
+		dbPath,
+	)
 	if err != nil {
 		return err
 	}
 
-	err = produceAndSendNotification(ctx, profile, &monetaryT)
-	return err
-}
-
-func extractAndReplaceMonetaryTransfer(
-	ctx context.Context,
-	id string,
-	mon *datastore.MonetaryTransfer,
-	networkPath string,
-) error {
-
-	// Use the debtor id to split the path
-	// so it gets the path next to the creditor ID
-	// to be replaced by the debtor ID, so it inserts on the correct collection.
-
-	//Split at gcpstuff in [0] and full db path in [1].
-	path := strings.Split(networkPath, "/documents/")[1]
-	//Split by collection and extract [1] which should be fromId.
-	fromId := strings.Split(path, "/")[1]
-	dbPath := strings.Replace(path, fromId, id, 1)
-
-	_, err := db.SetMonetaryTransfer(ctx, id, mon, dbPath)
+	err = produceAndSendNotification(
+		ctx,
+		profile,
+		&monetaryT,
+	)
 	return err
 }
 
@@ -68,7 +63,7 @@ func produceAndSendNotification(
 	transfer *datastore.MonetaryTransfer,
 ) error {
 	//generate notification message
-	token := profile.UserId.Token
+	token := profile.Token
 	message := messaging.GenerateRequestNotification(
 		token,
 		transfer.AmountUnit,
