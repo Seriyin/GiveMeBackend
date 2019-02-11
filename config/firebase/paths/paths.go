cae72831e165ed6ecd048909f6f707a67ee1a7dd
André Diogo <redan.blue27@gmail.com>
2019-02-11 21:44:00 +0000

    Adds path extraction methods with snowflake.

--- config/firebase/paths/paths.go
+++ config/firebase/paths/paths.go
@@ -22,6 +22,27 @@ func ExtractAndReplaceMethodIdAndDatePath(
 	return dbPath
 }
 
+func ExtractAndReplaceMethodIdAndDatePathWithSnowflake(
+	id string,
+	networkPath string,
+) (string, string) {
+
+	// Use the debtor id to split the path
+	// so it gets the path next to the creditor ID
+	// to be replaced by the debtor ID, so it inserts on the correct collection.
+
+	//Split at gcpstuff in [0] and full db path in [1].
+	path := strings.Split(networkPath, "/documents/")[1]
+	//Split by collection and extract [1] which should be fromId.
+	pathSplits := strings.Split(path, "/")
+	fromId := pathSplits[1]
+	//Extract [3] which is document unique id.
+	docId := pathSplits[3]
+	dbPath := strings.Replace(path, fromId, id, 1)
+
+	return docId, dbPath
+}
+
 func ExtractMethodIdAndDatePath(
 	networkPath string,
 ) string {
@@ -32,6 +53,20 @@ func ExtractMethodIdAndDatePath(
 
 	//Split at gcpstuff in [0] and full db path in [1].
 	return strings.Split(networkPath, "/documents/")[1]
+}
+
+func ExtractMethodIdAndDatePathWithSnowflake(
+	networkPath string,
+) (string, string) {
+
+	// Use the debtor id to split the path
+	// so it gets the path next to the creditor ID
+	// to be replaced by the debtor ID, so it inserts on the correct collection.
+
+	//Split at gcpstuff in [0] and full db path in [1].
+	path := strings.Split(networkPath, "/documents/")[1]
+	splits := strings.Split(path, "/")
+	return splits[3], path
 }
 
 func TransformGroupIntoMonetary(
