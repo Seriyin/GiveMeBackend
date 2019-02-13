2bf309bf9f90a8aa6d7ed0fbe3db43f39c9fd05b
André Diogo <redan.blue27@gmail.com>
2019-02-13 11:53:52 +0000

    Fixes wrong tag firebase -> firestore.

--- config/datastore/monetary.go
+++ config/datastore/monetary.go
@@ -5,34 +5,34 @@ import (
 )
 
 type MonetaryRequest struct {
-	From          string    `firebase:"from" json:"from"`
-	To            string    `firebase:"to" json:"to"`
-	Desc          string    `firebase:"desc" json:"desc"`
-	Date          time.Time `firebase:"date" json:"date"`
-	AmountUnit    int64     `firebase:"amountUnit" json:"amountUnit"`
-	AmountCents   int64     `firebase:"amountCents" json:"amountCents"`
-	Currency      string    `firebase:"currency" json:"currency"`
-	ConfirmedFrom bool      `firebase:"confirmedFrom" json:"confirmedFrom"`
-	ConfirmedTo   bool      `firebase:"confirmedTo" json:"confirmedTo"`
-	Snowflake     string    `firebase:"snowflake" json:"snowflake"`
-	GroupId       int64     `firebase:"groupId" json:"groupId"`
-	RecurrentId   int64     `firebase:"recurrentId" json:"recurrentId"`
+	From          string    `firestore:"from" json:"from"`
+	To            string    `firestore:"to" json:"to"`
+	Desc          string    `firestore:"desc" json:"desc"`
+	Date          time.Time `firestore:"date" json:"date"`
+	AmountUnit    int64     `firestore:"amountUnit" json:"amountUnit"`
+	AmountCents   int64     `firestore:"amountCents" json:"amountCents"`
+	Currency      string    `firestore:"currency" json:"currency"`
+	ConfirmedFrom bool      `firestore:"confirmedFrom" json:"confirmedFrom"`
+	ConfirmedTo   bool      `firestore:"confirmedTo" json:"confirmedTo"`
+	Snowflake     string    `firestore:"snowflake" json:"snowflake"`
+	GroupId       int64     `firestore:"groupId" json:"groupId"`
+	RecurrentId   int64     `firestore:"recurrentId" json:"recurrentId"`
 }
 
 type GroupRequest struct {
-	From        string    `firebase:"from" json:"from"`
-	Tos         []string  `firebase:"tos" json:"tos"`
-	Desc        string    `firebase:"desc" json:"desc"`
-	Date        time.Time `firebase:"date" json:"date"`
-	Included    bool      `firebase:"included" json:"included"`
-	AmountUnit  int64     `firebase:"amountUnit" json:"amountUnit"`
-	AmountCents int64     `firebase:"amountCents" json:"amountCents"`
-	Currency    string    `firebase:"currency" json:"currency"`
-	GroupId     int64     `firebase:"groupId" json:"groupId"`
+	From        string    `firestore:"from" json:"from"`
+	Tos         []string  `firestore:"tos" json:"tos"`
+	Desc        string    `firestore:"desc" json:"desc"`
+	Date        time.Time `firestore:"date" json:"date"`
+	Included    bool      `firestore:"included" json:"included"`
+	AmountUnit  int64     `firestore:"amountUnit" json:"amountUnit"`
+	AmountCents int64     `firestore:"amountCents" json:"amountCents"`
+	Currency    string    `firestore:"currency" json:"currency"`
+	GroupId     int64     `firestore:"groupId" json:"groupId"`
 }
 
 type RecurrentRequest struct {
-	RecurrentId int64 `firebase:"recurrent" json:"recurrent"`
-	Stamp       int64 `firebase:"stamp" json:"stamp"`
-	Concluded   bool  `firebase:"concluded" json:"concluded"`
+	RecurrentId int64 `firestore:"recurrent" json:"recurrent"`
+	Stamp       int64 `firestore:"stamp" json:"stamp"`
+	Concluded   bool  `firestore:"concluded" json:"concluded"`
 }
