f12c2d6e27849958b414af201e76385aa70688ea
André Diogo <redan.blue27@gmail.com>
2019-02-13 01:25:06 +0000

    Spaces are important.

--- config/firebase/firestore/monetary_test.go
+++ config/firebase/firestore/monetary_test.go
@@ -25,3 +25,17 @@ func TestTimeParsing(t *testing.T) {
 	}
 	return
 }
+
+func TestParseFromJSON(t *testing.T) {
+	ex := "{\"amountCents\":{\"integerValue\":\"52\"},\"amountUnit\":{\"integerValue\":\"432\"},\"confirmedFrom\":{\"booleanValue\":false},\"confirmedTo\":{\"booleanValue\":false},\"currency\":{\"stringValue\":\"€\"},\"date\":{\"timestampValue\":\"2019-02-13T00:21:13.036Z\"},\"desc\":{\"stringValue\":\"Woop\"},\"from\":{\"stringValue\":\"+351345345345\"},\"groupId\":{\"integerValue\":\"-1\"},\"recurrentId\":{\"integerValue\":\"-1\"},\"snowflake\":{\"stringValue\":\"3zwUD2mxrrsAs4QsDRP4\"},\"to\":{\"stringValue\":\"+351366366366\"}}"
+	var mon monetaryRequest
+	err := json.Unmarshal([]byte(ex), &mon)
+	if err != nil {
+		t.Errorf("err: %v", err)
+	} else {
+		bytes, err := json.Marshal(mon)
+		log.Printf("mon: %v", string(bytes))
+		log.Printf("err: %v", err)
+	}
+	return
+}
